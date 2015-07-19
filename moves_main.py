c428cf7e6a604ac27cabba118da98706f849c52a
Yihang Zhang <manbeck.cn@gmail.com>
2015-07-19 16:02:27 -0700

    fixed unit issues. Source type need to be modified

--- moves_main.py
+++ moves_main.py
@@ -26,17 +26,37 @@ import math
 #       self.a = a
 #       self.grad = grad
 
-def findSourceType(VehType):
+def findSourceType(VehType, M):
     '''
     Map VISSIM vehicle type into MOVES source type
+    VehType: VISSIM vehicle type number
+    M: vehicle weight in metric tons
     '''
     if 100 == VehType:
-        sourceType = 21
-    elif 200 == VehType:
-        sourceType = 62
-    elif 300 == VehType:
-        sourceType = 42
-    return sourceType
+        return 21
+    if 200 ==VehType:
+        if 15 < M < 25:
+            return 41
+        if 25 < M < 36:
+            return 62
+        if 3.8 < M < 6.3:
+            return 53
+        if 1.5 < M < 3.8:
+            return 32
+    if 300 == VehType:
+        return 42
+
+
+    # elif 200 == VehType and 15<M<22:
+    #     sourceType = 41
+    # elif 200 == VehType and 27<M<36:
+    #     sourceType = 62
+    # elif 200 == VehType and 3.8<M<6.3:
+    #     elif 
+    #     sourceType = 53
+    # elif 300 == VehType:
+    #     sourceType = 42
+    # return sourceType
 
 def findParameters(sourceType):
     '''Find vehicle parameters for specific source type'''
@@ -46,17 +66,35 @@ def findParameters(sourceType):
         C = 0.000493
         M = 1.4788
         f = 1.4788
-    elif 62 == sourceType:
-        A = 2.08126
-        B = 0
-        C = 0.004188
-        M = 31.4038
+    elif 32 == sourceType:
+        A = 0.235008
+        B = 0.003039
+        C = 0.000748
+        M = 2.05979
+        f = 2.05979        
+    elif 41 == sourceType:
+        A = 1.29515
+        B = 0        
+        C = 0.003715
+        M = 19.5937
         f = 17.1
     elif 42 == sourceType:
         A = 1.0944
         B = 0
         C = 0.003587
         M = 16.556
+        f = 17.1
+    elif 53 == sourceType:
+        A = 0.498699
+        B = 0
+        C = 0.001474
+        M = 6.25047
+        f = 17.1        
+    elif 62 == sourceType:
+        A = 2.08126
+        B = 0
+        C = 0.004188
+        M = 31.4038
         f = 17.1
     return A, B, C, M, f
 
@@ -71,7 +109,11 @@ def findVSP(A, B, C, M, f, v, a, grad):
 
 
 def findOperationMode(VSP, v, a):
-    '''Find operation mode of vehicle for current time'''
+    '''
+    Find operating mode of vehicle for current time
+    The operating mode number returned by this function is not MOVES operation mode ID, but the link ID 1~23, corresponding to 23 operation modes, instead.
+    This is for convenience in table lookup
+    '''
     if a <= -2:
         return 1
     elif v < 1 and v > -1:
@@ -110,6 +152,9 @@ def moves_main():
     '''
     Evaluate the total emission with VISSIM output *.fzp file and MOVES emission rate
     '''
+    #GRAVITY = 9.8   #gravitational acceleration in m/s^2
+    mph2ms = 0.44704    #convert mph to m/s
+    fts2ms = 0.3048 #convert ft/s^2 to m/s^2
     vehData = pd.read_csv('i710.fzp', skiprows = 21, sep = ';', skipinitialspace = True)
     # abandon strings in the last column
     vehData = vehData[list(vehData.columns[:-1])]
@@ -136,16 +181,20 @@ def moves_main():
         veh = vehData[i == vehData.VehNr]
         if 0 == len(veh):
             continue
+        throughput += 1
+        print throughput
         VehType = veh.Type.iloc[0]
-        sourceType = findSourceType(VehType)
+        M = veh.Weight.iloc[0] #Vehicle weight in metric tons
+        print VehType, M
+        sourceType = findSourceType(VehType, M)
         A, B, C, M, f = findParameters(sourceType)
         emissions = emissionRates[emissionRates.sourceTypeID == sourceType]
         for sec in range(len(veh)):
-            v = veh.iloc[sec]['v']
-            a = veh.iloc[sec]['a']
+            v = veh.iloc[sec]['v'] * mph2ms
+            a = veh.iloc[sec]['a'] * fts2ms
             grad = veh.iloc[sec]['Grad']
             VSP = findVSP(A, B, C, M, f, v, a, grad)
-            operationMode = findOperationMode(VSP, v, a)
+            operationMode = findOperationMode(VSP, v/mph2ms , a/mph2ms)
             VMT += veh.iloc[sec]['v'] / 3600
             travelTime += 1 / 60
             emission = emissions[emissions.linkID == operationMode]
@@ -158,7 +207,8 @@ def moves_main():
             pass
     rates = [HC, CO, NOX, CO2, energy, PM25]
     for i in len(rates):
-        rates[i] /= (VMT*nVeh)
+        rates[i] /= VMT
+    print rates, throughput
 
 
         #travelTime += (veh.iloc[-1]['t'] - veh.iloc[0]['t']) / 60
